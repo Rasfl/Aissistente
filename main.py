34c1104d29c25a7fa8d33f96f63d21d2557a9eca
Rasfl <rafav.lene@gmail.com>
2026-08-13 01:02:05 +0000

    V0.1.3: now Chatbot can track the user history, with the variable messages.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -7,23 +8,30 @@ from openai import OpenAI
 def main() -> None:
     load_dotenv()
 
+    messages: list[dict[str, str, str, object]] = [
+        {
+            "role": "user",
+            "content": args.user_prompt
+        }]
     # o uso do env_path é para que idependente onde o usuario esteja, ao rodar main.py, ele procura na pasta raiz de main.py
     api_key = os.environ.get("OPENROUTER_API_KEY")
     if not api_key:
         raise RuntimeError('OPENROUTER_API_KEY not set. Configure it in the environment or the expected dotenv file path.')
+    # ---
 
-# OpenAi Class and configs
-
+    # Parser with the user msg
+    parser = argparse.ArgumentParser(description="Aissistente")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    #---
+    # OpenAi Class and configs
     client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=api_key,)
 
     completion = client.chat.completions.create(
         model="deepseek/deepseek-v4-flash-0731",
-        messages=[
-        {
-            "role": "user", 
-            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."},
-        ],
+        messages= messages
     )
+    # ---
 
     # tokenUsage to track the consumption from the AI Tokens, also raise RuntimeError if usage == None
     def tokenUsage(completion) -> None:
@@ -35,6 +43,7 @@ def main() -> None:
         print(f"Prompt tokens: {promptTokens}")
         print(f"Response tokens: {completionTokens}")
     tokenUsage(completion)
+    # ---
 
     print("Reponse:")
     print(completion.choices[0].message.content)
