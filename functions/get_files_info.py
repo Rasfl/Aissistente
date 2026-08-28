f006898bbb4d8dff43838dc95e17b59d2dc5eec5
Rasfl <rafav.lene@gmail.com>
2026-08-28 19:13:08 +0000

    V0.2.1: Reogarnizado as pastas, além da criação do "get_files_info.py", com o proposito de enviar Diretorios para o Chatbot analisar, em fase de debugar.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,16 @@
 import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
-    absolutePath = os.path.abspath(working_directory)
-    target_dir = os.path.normapath(os.path.normpath(absolutePath, directory))
+    try:
+        absolutePath = os.path.abspath(working_directory)
+        targetDir = os.path.normpath(os.path.join(absolutePath, directory))
+        validTargetDir = os.path.commonpath([absolutePath, targetDir]) == absolutePath
+        isDir = os.path.isdir(directory)
+        if not validTargetDir:
+            return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        if not isDir:
+            return (f'Error: "{directory}" is not a directory')
+        else:
+            return f'Success: "{directory}" is within the working directory'
+    except Exception as e:
+        print(f"Error: '{e}'")
